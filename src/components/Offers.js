06d9923e742113d6f47a50e4e6b72651b7254e39
Kenneth Wilcox <kenneth.wilcox@purered.net>
2019-06-27 15:11:55 -0400

    hack to fix images

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -5,9 +5,18 @@ class OfferList extends Component {
       render() {
         
         let offersData = this.props.offerData.map((offer, index) => {
+          
+          //Just a freaking hack for the images to show...we need to fix.
+          if (offer.FPMain1_URL_PNG !== null) {
+            var imageName = offer.FPMain1_URL_PNG.substring(offer.FPMain1_URL_PNG.lastIndexOf('/') + 1);
+            console.log (imageName);
+          }
+          else {
+            return false;
+          }
           return(
             <Col xs="6" sm="4" className="offer text-center" key={offer.pKey}>
-              <img src={offer.FPMain1_URL_PNG} alt={offer.Alt_Text} style={{width: '150px', height: '200px'}}/>
+              <img src={`https://s3.wasabisys.com/hugo-images/2019/05/${imageName}`} alt={offer.Alt_Text} style={{width: '150px', height: '200px'}}/>
               <p className="promo__overline">{offer.Overline1}</p>
               <h5 className="font-weight-bold">{offer.Mainline1}</h5>
               <span className="promo_price">{offer.Price}</span>
@@ -16,6 +25,7 @@ class OfferList extends Component {
                 <Badge pill color="success" className="text-uppercase px-2 py-1 ml-3 mb-1 align-middle" style={{ fontSize: '0.75rem' }}>New</Badge>
               </span>
             </Col>
+            
           )
         })
     
