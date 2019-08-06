32ac2954ddd4f3670969d3b250c5dacb01881660
Bob Ware <bob.ware@purered.net>
2019-08-06 17:22:28 -0400

    Add latest production build

---
+++ build/precache-manifest.68c18e93b68c35d3f545adf60e237dc2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c46b64852ae5d128a0092460ff25867",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd68a42468e17da6a7c8",
+    "url": "/static/css/main.e9f97430.chunk.css"
+  },
+  {
+    "revision": "96d856b827f108824d3a",
+    "url": "/static/js/2.620e1a5a.chunk.js"
+  },
+  {
+    "revision": "dd68a42468e17da6a7c8",
+    "url": "/static/js/main.69a75341.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cbd387d93e253048800dc9ee22b85c24",
+    "url": "/static/media/fa-brands-400.cbd387d9.eot"
+  },
+  {
+    "revision": "d30c44aaad8efa7626428c8294f7e880",
+    "url": "/static/media/fa-brands-400.d30c44aa.woff"
+  },
+  {
+    "revision": "d6ac6c968cff1abcbf5d548828b9f6c6",
+    "url": "/static/media/fa-brands-400.d6ac6c96.ttf"
+  },
+  {
+    "revision": "eac60e8a656781e13d2a674b4d9051c0",
+    "url": "/static/media/fa-brands-400.eac60e8a.woff2"
+  },
+  {
+    "revision": "fef43c1fd872ef7f1880483d9b63ccf2",
+    "url": "/static/media/fa-brands-400.fef43c1f.svg"
+  },
+  {
+    "revision": "27603739fe78fee587c7b84873475b61",
+    "url": "/static/media/fa-solid-900.27603739.svg"
+  },
+  {
+    "revision": "2e302fa4c6eeb1bc06149067bae3e7b4",
+    "url": "/static/media/fa-solid-900.2e302fa4.eot"
+  },
+  {
+    "revision": "5dc01cfcd5336f696cb85da7ce53fa9b",
+    "url": "/static/media/fa-solid-900.5dc01cfc.woff2"
+  },
+  {
+    "revision": "80c404ff42e52d9e7589e83fe21307b4",
+    "url": "/static/media/fa-solid-900.80c404ff.ttf"
+  },
+  {
+    "revision": "a8eedaadb16b569a48a061d4aafa2d2e",
+    "url": "/static/media/fa-solid-900.a8eedaad.woff"
+  },
+  {
+    "revision": "28e944c5fa6b7c3b09974eb085ba035e",
+    "url": "/static/media/hugos-white-wood_944w.28e944c5.jpg"
+  }
+]);
