3267faf574b6162f8e4da6770d5e75cf512618f6
Rubayth Haque <rubayth.haque@purered.net>
2019-10-10 15:51:15 -0400

    Updates

---
+++ precache-manifest.907d9123b718b404d36aa2bc7eebed50.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7251b408ae3f3c7ef53ed31dbf5f218d",
+    "url": "/Digital-Circular/index.html"
+  },
+  {
+    "revision": "cb82be1df3c67ce70937",
+    "url": "/Digital-Circular/static/css/main.186e1b0a.chunk.css"
+  },
+  {
+    "revision": "ab1846a1f651755bc596",
+    "url": "/Digital-Circular/static/js/2.4d258e17.chunk.js"
+  },
+  {
+    "revision": "cb82be1df3c67ce70937",
+    "url": "/Digital-Circular/static/js/main.c867dfb2.chunk.js"
+  },
+  {
+    "revision": "d56bc42605de29f09eca",
+    "url": "/Digital-Circular/static/js/runtime~main.8360567b.js"
+  },
+  {
+    "revision": "cbd387d93e253048800dc9ee22b85c24",
+    "url": "/Digital-Circular/static/media/fa-brands-400.cbd387d9.eot"
+  },
+  {
+    "revision": "d30c44aaad8efa7626428c8294f7e880",
+    "url": "/Digital-Circular/static/media/fa-brands-400.d30c44aa.woff"
+  },
+  {
+    "revision": "d6ac6c968cff1abcbf5d548828b9f6c6",
+    "url": "/Digital-Circular/static/media/fa-brands-400.d6ac6c96.ttf"
+  },
+  {
+    "revision": "eac60e8a656781e13d2a674b4d9051c0",
+    "url": "/Digital-Circular/static/media/fa-brands-400.eac60e8a.woff2"
+  },
+  {
+    "revision": "fef43c1fd872ef7f1880483d9b63ccf2",
+    "url": "/Digital-Circular/static/media/fa-brands-400.fef43c1f.svg"
+  },
+  {
+    "revision": "27603739fe78fee587c7b84873475b61",
+    "url": "/Digital-Circular/static/media/fa-solid-900.27603739.svg"
+  },
+  {
+    "revision": "2e302fa4c6eeb1bc06149067bae3e7b4",
+    "url": "/Digital-Circular/static/media/fa-solid-900.2e302fa4.eot"
+  },
+  {
+    "revision": "5dc01cfcd5336f696cb85da7ce53fa9b",
+    "url": "/Digital-Circular/static/media/fa-solid-900.5dc01cfc.woff2"
+  },
+  {
+    "revision": "80c404ff42e52d9e7589e83fe21307b4",
+    "url": "/Digital-Circular/static/media/fa-solid-900.80c404ff.ttf"
+  },
+  {
+    "revision": "a8eedaadb16b569a48a061d4aafa2d2e",
+    "url": "/Digital-Circular/static/media/fa-solid-900.a8eedaad.woff"
+  },
+  {
+    "revision": "28e944c5fa6b7c3b09974eb085ba035e",
+    "url": "/Digital-Circular/static/media/hugos-white-wood_944w.28e944c5.jpg"
+  }
+]);
