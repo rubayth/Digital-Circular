74a86968d49f860eb0aa3d9132c5d5010b55ccdc
rubayth <rubayth.haque@purered.net>
2019-10-11 22:48:25 -0400

    set cookie before fetch

--- src/App.js
+++ src/App.js
@@ -20,9 +20,7 @@ class App extends Component {
         <Container className="circular-container">
           <FilterContainer />
           {this.props.cookies.get("store") ? (
-            <div>
               <OfferList />
-            </div>
           ) : (
             <Landing />
           )}
